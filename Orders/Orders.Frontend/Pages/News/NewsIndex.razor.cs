82e72fd49ff769057b37b3a6536c163ea0225ed9
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-06-02 16:41:21 -0500

    News index

---
+++ Orders/Orders.Frontend/Pages/News/NewsIndex.razor.cs
@@ -0,0 +1,187 @@
+using Blazored.Modal.Services;
+using Blazored.Modal;
+using CurrieTechnologies.Razor.SweetAlert2;
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Orders.Frontend.Pages.Banks;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.News
+{
+    [Authorize(Roles = "Admin")]
+    public partial class NewsIndex
+    {
+        public List<NewsArticle>? NewsArticles { get; set; }
+
+        private MudTable<NewsArticle> table = new();
+        private readonly int[] pageSizeOptions = { 10, 25, 50, 5, int.MaxValue };
+        private int totalRecords = 0;
+        private bool loading;
+        private const string baseUrl = "api/news";
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        [Parameter, SupplyParameterFromQuery] public string Filter { get; set; } = string.Empty;
+        [CascadingParameter] private IModalService Modal { get; set; } = default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync()
+        {
+            await LoadTotalRecords();
+        }
+
+        private async Task<bool> LoadTotalRecords()
+        {
+            loading = true;
+            var url = $"{baseUrl}/recordsnumber?page=1&recordsnumber={int.MaxValue}";
+
+            if (!string.IsNullOrWhiteSpace(Filter))
+            {
+                url += $"&filter={Filter}";
+            }
+
+            var responseHttp = await Repository.GetAsync<int>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync(new SweetAlertOptions
+                {
+                    Title = "Error",
+                    Text = message,
+                    Icon = SweetAlertIcon.Error
+                });
+                return false;
+            }
+            totalRecords = responseHttp.Response;
+            loading = false;
+            return true;
+        }
+
+        private async Task<TableData<NewsArticle>> LoadListAsync(TableState state)
+        {
+            int page = state.Page + 1;
+            int pageSize = state.PageSize;
+            var url = $"{baseUrl}?page={page}&recordsnumber={pageSize}";
+
+            if (!string.IsNullOrWhiteSpace(Filter))
+            {
+                url += $"&filter={Filter}";
+            }
+
+            if (!string.IsNullOrWhiteSpace(Filter))
+            {
+                url += $"&filter={Filter}";
+            }
+
+            var responseHttp = await Repository.GetAsync<List<NewsArticle>>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync(new SweetAlertOptions
+                {
+                    Title = "Error",
+                    Text = message,
+                    Icon = SweetAlertIcon.Error
+                });
+                return new TableData<NewsArticle> { Items = [], TotalItems = 0 };
+            }
+            if (responseHttp.Response == null)
+            {
+                return new TableData<NewsArticle> { Items = [], TotalItems = 0 };
+            }
+            return new TableData<NewsArticle>
+            {
+                Items = responseHttp.Response,
+                TotalItems = totalRecords
+            };
+        }
+
+        private async Task SetFilterValue(string value)
+        {
+            Filter = value;
+            await LoadAsync();
+            await table.ReloadServerData();
+        }
+
+        private async Task ShowModalAsync(int id = 0, bool isEdit = false)
+        {
+            IModalReference modalReference;
+
+            if (isEdit)
+            {
+                modalReference = Modal.Show<BankEdit>(string.Empty, new ModalParameters().Add("Id", id));
+            }
+            else
+            {
+                modalReference = Modal.Show<BankCreate>();
+            }
+
+            var result = await modalReference.Result;
+            if (result.Confirmed)
+            {
+                await LoadAsync();
+            }
+            await table.ReloadServerData();
+        }
+
+        private async Task DeleteAsync(NewsArticle newsArticle)
+        {
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "¿Estás seguro?",
+                Text = $"¿Estás seguro de que quieres eliminar la noticia: {newsArticle.Title}?",
+                Icon = SweetAlertIcon.Warning,
+                ShowCancelButton = true,
+            });
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+
+            var responseHttp = await Repository.DeleteAsync<NewsArticle>($"api/news/{newsArticle.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("/newsArticles");
+                }
+                else
+                {
+                    var message = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync(new SweetAlertOptions
+                    {
+                        Title = "Error",
+                        Text = message,
+                        Icon = SweetAlertIcon.Error
+                    });
+                }
+                return;
+            }
+            await LoadAsync();
+            await table.ReloadServerData();
+            ShowToast("Ok", SweetAlertIcon.Success, "Banco eliminado.");
+        }
+
+        private void ShowToast(string title, SweetAlertIcon iconMessage, string message)
+        {
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            _ = toast.FireAsync(title, message, iconMessage);
+        }
+    }
+}
