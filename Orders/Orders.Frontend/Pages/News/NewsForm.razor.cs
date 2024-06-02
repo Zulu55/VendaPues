caf9a0c60ec5e20444c04475d951bb741f9e2ff5
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-06-02 17:42:10 -0500

    WIP

---
+++ Orders/Orders.Frontend/Pages/News/NewsForm.razor.cs
@@ -0,0 +1,75 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+using Orders.Shared.DTOs;
+
+namespace Orders.Frontend.Pages.News
+{
+    public partial class NewsForm
+    {
+        private EditContext editContext = null!;
+        private string? imageUrl;
+
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        [Parameter, EditorRequired] public NewsArticle NewsArticle { get; set; } = null!;
+        [Parameter, EditorRequired] public EventCallback OnValidSubmit { get; set; }
+        [Parameter, EditorRequired] public EventCallback ReturnAction { get; set; }
+        [Parameter] public bool IsEdit { get; set; } = false;
+
+        public bool FormPostedSuccessfully { get; set; } = false;
+        private string titleLabel => IsEdit ? "Editar Noticia" : "Crear Noticia";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            editContext = new(NewsArticle);
+        }
+
+        private void ImageSelected(string imagenBase64)
+        {
+            imageUrl = null;
+        }
+
+        private async Task OnDataAnnotationsValidatedAsync()
+        {
+            await OnValidSubmit.InvokeAsync();
+        }
+
+        private async Task OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            var formWasEdited = editContext.IsModified();
+
+            if (!formWasEdited)
+            {
+                return;
+            }
+
+            if (FormPostedSuccessfully)
+            {
+                return;
+            }
+
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Deseas abandonar la página y perder los cambios?",
+                Icon = SweetAlertIcon.Warning,
+                ShowCancelButton = true
+            });
+
+            var confirm = !string.IsNullOrEmpty(result.Value);
+
+            if (confirm)
+            {
+                return;
+            }
+
+            context.PreventNavigation();
+        }
+    }
+}
